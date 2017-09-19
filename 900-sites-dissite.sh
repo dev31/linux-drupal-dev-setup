f6f58ea288d39a8fea8b920a5efcd56be8eeaf5a
Mike Shiyan <dev@mike.pp.ua>
2017-09-19 20:11:59 +0300

    SSL for sites

--- 900-sites-dissite.sh
+++ 900-sites-dissite.sh
@@ -2,6 +2,7 @@
 
 NAME=$1
 
+sudo rm /etc/ssl/private/$NAME.crt
 sudo mono /usr/local/exe/hostscmd/hosts.exe rem $NAME >/dev/null
 sudo a2dissite -q $NAME
 sudo systemctl reload apache2
--- 900-sites-ensite.sh
+++ 900-sites-ensite.sh
@@ -9,6 +9,23 @@ then
   exit 1
 fi
 
+OUT=`mktemp`
+TMP=`mktemp`
+CRT="/etc/ssl/private/$NAME.crt"
+CNF="/usr/share/ssl-cert/ssleay.cnf"
+
+sed -e s#@HostName@#"$NAME"# $CNF > $TMP
+echo "subjectAltName=DNS:$NAME" >> $TMP
+
+if ! sudo openssl req -config $TMP -new -x509 -days 3650 -nodes -sha256 \
+  -out $CRT -keyout $CRT > $OUT 2>&1
+then
+  echo Error: Could not create certificate. Openssl output was: >&2
+  cat $OUT >&2
+  exit 1
+fi
+sudo chmod 0600 $CRT
+
 TMP=`mktemp`
 
 TPL=`cat /etc/ensite/vhost.conf.template`
